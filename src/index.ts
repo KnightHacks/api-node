f3b254fc8b33e5564cd42a6f3aeed500065d1f41
suneettipirneni <suneettipirneni@icloud.com>
2021-08-30 14:29:35 -0400

    export types

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
 export * from './controllers/category';
 export * from './controllers/event';
 export * from './API';
+export * from './models/category';
+export * from './models/event';
+export * from './models/group';
+export * from './models/user';

afd2f2064a62bb7df4fbab8d5a86153c0a645e08
suneettipirneni <suneettipirneni@icloud.com>
2021-09-05 00:50:25 -0400

    add missing change

--- src/controllers/event.ts
+++ src/controllers/event.ts
@@ -1,5 +1,5 @@
 import { Endpoints } from '../Endpoints';
-import { APIEvent, Event, transformAPIEvent } from '../models/event';
+import { APIEventData, EventData, transformAPIEvent } from '../models/event';
 import { emptyCollectionHandler } from '../util/api';
 import { BaseManager } from './base';
 
@@ -8,10 +8,10 @@ export class EventManager extends BaseManager {
    * Retrieves all of the hackathon events.
    * @returns A list of the hackathon events.
    */
-  async fetchAll(): Promise<Event[]> {
+  async fetchAll(): Promise<EventData[]> {
     const response = (await this.rest
       .performRequest(Endpoints.getAllEvents)
-      .catch(emptyCollectionHandler)) as { events: APIEvent[] };
+      .catch(emptyCollectionHandler)) as { events: APIEventData[] };
     return response.events.map(transformAPIEvent);
   }
 }
